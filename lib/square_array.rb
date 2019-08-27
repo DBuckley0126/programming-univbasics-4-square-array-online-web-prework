1ca05faef1807743b4641239757e55fab0c35495
Danny Buckley <dannyboy.msn@hotmail.com>
2019-08-27 16:03:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,13 @@
 def square_array(array)
   
   counter = 0
-  square_array[]
+  new_numbers[]
   
   while array[counter] do
     
-    square_array.push(array[counter] ** 2)
+    new_numbers.push(array[counter] ** 2)
     counter += 1
   
   end
-  
+  new_numbers
 end
